d37d68afdc6f4f8a0ed74ac4e493b5181b061b5d
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-01-16 12:12:39 +0000

    create tests for check_codeword_spec.rb

---
+++ testing-bites/spec/check_codeword_spec.rb
@@ -0,0 +1,19 @@
+require "check_codeword"
+
+RSpec.describe"check_codeword method" do
+  it "Rejects a user if password is wrong" do
+    result = check_codeword("ADWDA")
+    expect(result).to eq "WRONG!"
+  end
+
+  it "Accepts a user if password is correct" do
+    result = check_codeword("horse")
+    expect(result).to eq "Correct! Come in."
+  end
+
+  it "Tells a user their guess was close" do
+    result = check_codeword("hope")
+    expect(result).to eq "Close, but nope."
+  end
+
+end
