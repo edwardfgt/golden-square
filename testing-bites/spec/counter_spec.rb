385be17a926ad80caf394977edddf66e12a12ecb
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-01-16 13:54:27 +0000

    add new test cases for counter_spec.rb

--- testing-bites/spec/counter_spec.rb
+++ testing-bites/spec/counter_spec.rb
@@ -9,8 +9,17 @@ RSpec.describe Counter do
   end
   
   it "reports the starting count of 0" do
-    counter=Counter.new
+    counter = Counter.new
     result = counter.report
     expect(result).to eq "Counted to 0 so far."
+  end
+
+  it "reports the current count after multiple added values" do
+    counter = Counter.new
+    counter.add(5)
+    counter.add(3)
+    counter.add(10)
+    result = counter.report
+    expect(result).to eq "Counted to 18 so far."
   end
 end
