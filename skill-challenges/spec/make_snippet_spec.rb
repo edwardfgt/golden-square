7987bd15f388216ab8cac9e7da469fb7aa124068
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-01-17 12:23:30 +0000

    create make_snippet method using tdd, create make_snippet.rb tests

---
+++ skill-challenges/spec/make_snippet_spec.rb
@@ -0,0 +1,33 @@
+require 'make_snippet'
+
+RSpec.describe "make_snippet method" do
+  context "when string is empty" do
+    it "returns an empty string" do
+      result = make_snippet("")
+      expect(result).to eq ""
+    end
+  end
+
+  context "given a string which is fewer than 5 words" do
+    it "returns the string" do
+      result = make_snippet("hello everyone goodbye everyone")
+      expect(result).to eq "hello everyone goodbye everyone"
+    end
+  end
+
+  context "given a string of five words" do
+    it "returns the string" do
+      result = make_snippet("hello everyone goodbye everyone hello")
+      expect(result).to eq "hello everyone goodbye everyone hello"
+    end
+  end
+
+  context "given a string of six words" do
+    it "returns the first 5 words, followed by a ..." do
+      result = make_snippet("hello everyone goodbye everyone hello everyone")
+      expect(result).to eq "hello everyone goodbye everyone hello..."
+    end
+  end
+
+
+end
